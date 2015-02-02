ce18ca8e14d36c2638d4e1f6e406283d7f436999
tylernickr <tylernickr@gmail.com>
2015-02-02 15:34:22 -0500

    Removed Action Bar

--- app/src/main/java/com/ntyler/haveyoubeentopunters/MainActivity.java
+++ app/src/main/java/com/ntyler/haveyoubeentopunters/MainActivity.java
@@ -1,20 +1,20 @@
 package com.ntyler.haveyoubeentopunters;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity implements OnClickListener {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*savedInstanceState().requestFeature(Window.FEATURE_ACTION_BAR);
-        getActionBar().hide();*/
+
         setContentView(R.layout.activity_main);
 
         View puntsButton = findViewById(R.id.punts_button);
