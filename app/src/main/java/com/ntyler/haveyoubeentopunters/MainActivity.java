969e1faf5ef7217a8f901b1beddb41f99f3f8bb5
tylernickr <tylernickr@gmail.com>
2015-02-02 15:19:06 -0500

    Layout Created and Wired Up

--- app/src/main/java/com/ntyler/haveyoubeentopunters/MainActivity.java
+++ app/src/main/java/com/ntyler/haveyoubeentopunters/MainActivity.java
@@ -1,39 +1,32 @@
 package com.ntyler.haveyoubeentopunters;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /*savedInstanceState().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();*/
         setContentView(R.layout.activity_main);
+
+        View puntsButton = findViewById(R.id.punts_button);
+        puntsButton.setOnClickListener(this);
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.punts_button:
+                Toast.makeText(getApplicationContext(), "You're in Punter\'s, Yay!",
+                        Toast.LENGTH_SHORT).show();
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
