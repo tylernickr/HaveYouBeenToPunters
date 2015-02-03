593e1d75c40ef0a39aad1620958cb25e6f6eba5b
tylernickr <tylernickr@gmail.com>
2015-02-02 22:55:00 -0500

    Quick Commit For Kevin

--- app/src/main/java/com/ntyler/haveyoubeentopunters/MainActivity.java
+++ app/src/main/java/com/ntyler/haveyoubeentopunters/MainActivity.java
@@ -2,31 +2,74 @@ package com.ntyler.haveyoubeentopunters;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.app.ActionBar;
+import android.text.format.Time;
+import android.util.Log;
+import android.widget.TextView;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
-import android.widget.Toast;
 
 
 public class MainActivity extends Activity implements OnClickListener {
+    private static final String TAG = "Main_Screen";
+
+    Time mCurrentTime;
+    Time mLastPuntsVisit;
+    TextView mPuntsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
 
         setContentView(R.layout.activity_main);
 
+        mLastPuntsVisit = new Time();
+        if ( savedInstanceState != null ) {
+            mLastPuntsVisit.set(savedInstanceState.getLong("puntsTime"));
+        }
+        mPuntsText = (TextView) findViewById(R.id.punts_time);
+
         View puntsButton = findViewById(R.id.punts_button);
         puntsButton.setOnClickListener(this);
+        mCurrentTime = new Time();
+        mCurrentTime.setToNow();
 
+
+    }
+
+    @Override
+    protected  void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        mCurrentTime.setToNow();
+        if (mLastPuntsVisit != null) {
+            double currentTime = mCurrentTime.toMillis(false);
+            double lastPuntsVisit = mLastPuntsVisit.toMillis(false);
+            double hoursSince = ((currentTime - lastPuntsVisit) / 3600000.0);
+            mPuntsText.setText("Hours: " + ((Double)hoursSince).toString());
+        }
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.punts_button:
-                Toast.makeText(getApplicationContext(), "You're in Punter\'s, Yay!",
-                        Toast.LENGTH_SHORT).show();
+                mCurrentTime.setToNow();
+                mLastPuntsVisit.setToNow();
+                if (mLastPuntsVisit != null) {
+                    double currentTime = mCurrentTime.toMillis(false);
+                    double lastPuntsVisit = mLastPuntsVisit.toMillis(false);
+                    double hoursSince = ((currentTime - lastPuntsVisit) / 3600000.0);
+                    String result = ((Double)hoursSince).toString();
+                    mPuntsText.setText("Hours: " + result);
+                }
+
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        Log.d(TAG, "Saving Bundle");
+        super.onSaveInstanceState(state);
+        state.putLong("puntsTime", mLastPuntsVisit.toMillis(true));
     }
 }
